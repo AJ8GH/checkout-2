2a5939463d3888ebcfdfd63335ae1e4e1fd9d185
Adam Jonas <adamjonas25@gmail.com>
2021-01-22 21:15:41 +0000

    create store class, pass first spec

---
+++ lib/store.rb
@@ -0,0 +1,4 @@
+class Store
+  def initialize(args = {})
+  end
+end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,7 +1,7 @@
 describe Store do
   context '#intialize' do
     it '#takes a hash as an argument' do
-      expect(Store.new({})).to_not raise_error
+      expect{ Store.new({}) }.to_not raise_error
     end
   end
 end
