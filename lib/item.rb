7e76b3869563388793e5e21b815eccd01fdc02ff
Adam Jonas <adamjonas25@gmail.com>
2021-01-24 16:14:19 +0000

    refactor Item initialize method to DRY it out

--- lib/item.rb
+++ lib/item.rb
@@ -3,7 +3,7 @@ class Item
   attr_reader :name, :price, :scan_price
   def initialize(args = {})
     @name = args.fetch(:name).to_string.capitalize
-    @price = args.fetch(:price).pounds
     @scan_price = args.fetch(:price)
+    @price = scan_price.pounds
   end
 end
