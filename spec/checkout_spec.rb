251f9b70c5e167f604ff04692e6ed36f9973f604
Adam Jonas <adamjonas25@gmail.com>
2021-01-24 12:11:14 +0000

    pass spec for basket getter method

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,7 +1,14 @@
 describe Checkout do
+  let (:checkout) { Checkout.new([]) }
   context '#initialize' do
     it 'takes a basket array as argument' do
-      expect { Checkout.new([]) }.to_not raise_error 
+      expect { Checkout.new([]) }.to_not raise_error
+    end
+  end
+
+  context '#basket' do
+    it '#shows the basket of items' do
+      expect(checkout.basket).to eq []
     end
   end
 end
