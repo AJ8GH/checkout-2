4d3cc183291eb6b640b8b9032997831329b3e003
Adam Jonas <adamjonas25@gmail.com>
2021-01-24 15:10:58 +0000

    refactor scan item into multiple methods

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -6,15 +6,27 @@ class Checkout
   end
 
   def total_price
-    scan_total.pounds
+    scan_total.poundss
   end
 
   def scan(item_name)
-    index = basket.find_index { |item| item.name == item_name.capitalize }
-    price, name = basket[index].price, basket[index].name
+    index = get_item_index(item_name)
+    price = get_item_price(index)
     self.total += basket[index].scan_price
+    remove_item(index)
+    "#{item_name.capitalize}: #{price}, total: #{total}"
+  end
+
+  def get_item_index(item_name)
+    basket.find_index { |item| item.name == item_name.capitalize }
+  end
+
+  def get_item_price(index)
+    basket[index].price
+  end
+
+  def remove_item(index)
     basket.delete_at(index)
-    "#{name}: #{price}, total: #{total}"
   end
 
 end
